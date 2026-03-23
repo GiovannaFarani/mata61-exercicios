78bfc05f1961d5304f3e79116032c0eafed3001e
Giovanna Farani <38029781-gifarani@users.noreply.replit.com>
2026-03-23 10:42:21 +0000

    Exercício 1 compiladores

--- E1/main.c
+++ E1/main.c
@@ -1,6 +1,23 @@
 #include "token.h"
+#include <stdio.h>
+
+extern int yylex();
+extern char* yytext;
 
 int main(int argc, char **argv) {
+    int token;
 
+  while ((token = yylex()) != 0){
+    if (token == NUM){
+      printf("categoria do token: %d, constante numérica: %s\n", token, yytext);
+    }
+    else if (token == ERROR){
+      printf("erro léxico: caractere inválido\n");
+    }
+    else{
+      printf("categoria do token: %d\n", token);
+    }
+  }
+  return 0;
 }
 
